f07519f37fad9d135d5af6946d6bd7397ed7f4fd
李小海 <1240141465@qq.com>
2022-02-27 18:13:35 +0800

    product

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -3,4 +3,6 @@ export const TIME_OUT = 5000
 
 if(process.env.NODE_ENV === 'development'){
     BASE_URL = 'http://120.27.159.12:8000'
+}else if(process.env.NODE_ENV === 'production'){
+    BASE_URL = 'http://120.27.159.12:8000'
 }
